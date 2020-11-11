bd48dad38deecde95460b948edec94613830a5dc
codigube <codigube@gmail.com>
2020-11-11 15:18:12 +0100

    Added Terraform code for testing CI/CD

---
+++ terraform/dev/demo-cicd/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.12.7, < 0.14"
+
+  required_providers {
+    aws = ">= 2.68, < 4.0"
+  }
+
+  backend "s3" {
+    key            = "concourse-demo/dev/terraform.tfstate"
+    bucket         = "tf-backend-terraform-state"
+    dynamodb_table = "tf-backend-terraform-state"
+    acl            = "bucket-owner-full-control"
+    region         = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+locals {
+  environment = "dev"
+  name_prefix = "concourse-demo"
+
+  tags = {
+    terraform   = "True"
+    environment = local.environment
+  }
+}
+# test
+module "demo-cicd" {
+  name_prefix = "${local.name_prefix}-${local.environment}"
+  source      = "../../modules/demo-cicd"
+}
